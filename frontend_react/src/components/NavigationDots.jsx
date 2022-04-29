6cd94a68b58dfe8cb9c95c6d43c3b767055963cd
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-04-29 11:52:56 +0200

    Minor styling changes

--- frontend_react/src/components/NavigationDots.jsx
+++ frontend_react/src/components/NavigationDots.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const NavigationDots = ({ active }) => {
   return (
     <div className='app__navigation'>
-      {['home', 'about', 'work', 'skills', 'testimonials', 'contact' ].map((item, index) => (
+      {['home', 'about', 'work', 'skills', 'contact' ].map((item, index) => (
         <a 
           href={`#${item}`}
           key={item + index}
           className="app__navigation-dot" 
-          style={active === item ? { animation: 'spin 5s infinite linear' } : {} } 
+          style={active === item ? { backgroundColor: '#A2B38B' } : {} } 
         />
       ))}
 
