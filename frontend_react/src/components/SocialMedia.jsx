60c7661e37858852655da6b58ce736e366ec02c7
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-05-03 17:58:43 +0200

    Change div to anchor-tag to make it clickable

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,18 +1,15 @@
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
-import { FaFacebookF } from "react-icons/fa";
+import { BsGithub, BsLinkedin } from 'react-icons/bs';
+import { SiNetlify } from "react-icons/si";
 
 const SocialMedia = () => {
   return (
-    <div className='app__social'>
-        <div>
-            <BsTwitter />
-        </div>
-        <div>
-            <FaFacebookF />
-        </div>
-        <div>
-            <BsInstagram />
-        </div>
+    <div className='app__social' target="_blank" rel="noopener noreferrer">
+        <a target="_blank" rel="noopener noreferrer" href='https://github.com/RubenDelb'>
+            <BsGithub />
+        </a>
+        <a target="_blank" rel="noopener noreferrer" href='https://www.linkedin.com/in/ruben-delbrassine-ab861888/'>
+            <BsLinkedin />
+        </a>
     </div>
   )
 }
