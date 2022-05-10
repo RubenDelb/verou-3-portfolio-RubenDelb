6a5851cab665c5b868f308b475caee6e7a4a2fab
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-05-10 13:02:05 +0200

    Change img-format to webp for better performance

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -1,25 +1,20 @@
-import email from '../assets/email.png';
-import mobile from '../assets/mobile.png';
-import css from '../assets/css.png';
-import git from '../assets/git.png';
-import html from '../assets/html.png';
-import javascript from '../assets/javascript.png';
-import node from '../assets/node.png';
-import react from '../assets/react.png';
-import sass from '../assets/sass.png';
-import typescript from '../assets/typescript.png';
+import email from '../assets/email.webp';
+import css from '../assets/css.webp';
+import git from '../assets/git.webp';
+import html from '../assets/html.webp';
+import javascript from '../assets/javascript.webp';
+import node from '../assets/node.webp';
+import react from '../assets/react.webp';
+import sass from '../assets/sass.webp';
+import typescript from '../assets/typescript.webp';
 import laravel from '../assets/laravel.svg';
 
-import about01 from '../assets/about01.png';
-import about02 from '../assets/about02.png';
-
-import profile from '../assets/profile.png';
-import logo from '../assets/logo.png';
-import speechBubble from '../assets/speechBubble.png';
+import profile from '../assets/profile.webp';
+import logo from '../assets/logo.webp';
+import speechBubble from '../assets/speechBubble.webp';
 
 export default {
     email,
-    mobile,
     css,
     git,
     html,
@@ -28,8 +23,6 @@ export default {
     react,
     sass,
     typescript,
-    about01,
-    about02,
     profile,
     logo,
     laravel,
