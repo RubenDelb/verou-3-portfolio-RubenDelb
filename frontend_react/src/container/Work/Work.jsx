bfe097c5f15cdcc23c45c27703aa923203086fa2
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-05-10 13:00:16 +0200

    Add aria-label to a-tag + add correct alt-tag on img

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -42,7 +42,7 @@ const Work = () => {
       <h2 className="head-text">My creative <span>Portfolio </span>Section</h2>,
 
       <div className="app__work-filter">
-        {['UI/UX', 'Web App', 'Mobile App', 'React JS', 'All'].map((item, index) => (
+        {['Laravel', 'React JS', 'All'].map((item, index) => (
           <div
             key={index}
             onClick={() => handleWorkFilter(item)}
@@ -61,14 +61,14 @@ const Work = () => {
         { filterWork.map((work, index) => (
           <div className="app__work-item app__flex" key={index}>
             <div className="app__work-img app__flex">
-              <img src={urlFor(work.imgUrl)} alt={work.name} />
+              <img src={urlFor(work.imgUrl)} alt={`${work.title}`} />
 
               <motion.div
                 whileHover={{ opacity: [0, 1] }}
                 transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5}}
                 className="app__work-hover app__flex"
               >
-                <a href={work.projectLink} target="_blank" rel="noreferrer">
+                <a href={work.projectLink} aria-label={work.title} target="_blank" rel="noreferrer">
                   <motion.div 
                     whileInView={{ scale: [0, 1] }}
                     whileHover={{ scale: [1, 0.9] }}
@@ -78,7 +78,7 @@ const Work = () => {
                     <AiFillEye />
                   </motion.div>
                 </a>
-                <a href={ work.codeLink } target="_blank" rel="noreferrer">
+                <a href={ work.codeLink } aria-label={work.title} target="_blank" rel="noreferrer">
                   <motion.div 
                     whileInView={{ scale: [0, 1] }}
                     whileHover={{ scale: [1, 0.9] }}
